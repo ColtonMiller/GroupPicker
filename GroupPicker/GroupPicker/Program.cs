f41a428a838b186dd5628721e8e3cb67ab7adc5f
Colton Miller <ColtonJMMiller@gmail.com>
2015-04-10 07:45:43 -0600

    Running without errors

--- GroupPicker/GroupPicker/Program.cs
+++ GroupPicker/GroupPicker/Program.cs
@@ -11,20 +11,38 @@ namespace GroupPicker
         static List<string> studentList = new List<string> { "Nathan", "Mahmoud", "Lamond", "Mac", "Colton", "Ryan", "Umar", "Kris", "David", "Mitch", "Matt", "Aaron", "Keith" };
         static void Main(string[] args)
         {
-        
+            PickGroup(studentList, 4);
+            Console.ReadKey();
         }
-        public static void PickGroup(List<string> classOfStudents, int groupSize)
+        /// <summary>
+        /// Picks a groups at random from a list of students
+        /// </summary>
+        /// <param name="classOfstudents">List of students</param>
+        /// <param name="groupSize">The size of a group</param>
+        public static void PickGroup(List<string> studentList, int groupSize)
         {
             List<string> currentGroupList = new List<string> { };
             int groupNumber = 1;
             Random randomStudent = new Random();
             while (studentList.Count > 0)
             {
+                string currentStudent = studentList[randomStudent.Next(0, studentList.Count)];
+                currentGroupList.Add(currentStudent);
+                studentList.Remove(currentStudent);
+                if (currentGroupList.Count == groupSize || studentList.Count == 0)
+                {
+                    Console.WriteLine("Group {0}", groupNumber);
+                    for (int i = 0; i < currentGroupList.Count; i++)
+                    {
+                        Console.WriteLine(currentGroupList[i]);
+                    }
+                    Console.WriteLine();
+                    currentGroupList.RemoveRange(0, currentGroupList.Count);
+                    groupNumber++;
 
+                }
             }
-                
-                
-            
         }
     }
 }
+
